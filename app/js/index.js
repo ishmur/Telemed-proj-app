ea7493d1105b9d235f24319e5076b34341ee0fc4
ishmur <mateuszpab@gmail.com>
2016-04-04 21:34:33 +0200

    Changed index.html layout
    Added image (glyph), title jumbotron, login button and login modal

--- app/js/index.js
+++ app/js/index.js
@@ -4,28 +4,25 @@
 var modalLogin = document.getElementById("myModal-login");
 var modalDialogLogin = document.getElementsByClassName("modal-dialog")[0];
 var btnLogin = document.getElementById("myBtn-login");
-var span = document.getElementsByClassName("close")[0];
+var spanCloseLogin = document.getElementsByClassName("close")[0];
 
 function resizeModal(modajDialogObj) {
   modajDialogObj.style['margin-top'] = (window.innerHeight - modajDialogObj.clientHeight)/2 + "px";
 }
 
-// When the user clicks the button, open the modal
+window.addEventListener("resize", resizeModal(modalDialogLogin));
+
 btnLogin.onclick = function() {
     modalLogin.style.display = "block";
     resizeModal(modalDialogLogin);
 }
 
-window.addEventListener("resize", resizeModal(modalDialogLogin));
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+spanCloseLogin.onclick = function() {
     modalLogin.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modalLogin) {
         modalLogin.style.display = "none";
     }
 }
