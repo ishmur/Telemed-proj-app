4fd74f7d8d6bd802a2d5039d2d1713ea3a7e1a20
ishmur <mateuszpab@gmail.com>
2016-04-06 00:20:05 +0200

    Some work on fixing navbar

--- app/js/home.js
+++ app/js/home.js
@@ -5,6 +5,8 @@ const ipcRenderer = require('electron').ipcRenderer;
 
 var btnHome = document.getElementById("btnHome");
 var bodyTag = document.getElementById("bodyTag");
+var divNav = document.getElementById("myNavbar");
+var btnNav = document.getElementsByClassName("navbar-toggle")[0];
 
 window.onload = function() {
   setTimeout(function() {
@@ -12,9 +14,19 @@ window.onload = function() {
   }, fadeLib.fadeInDelay);
 }
 
-btnHome.addEventListener('click', function () {
-    fadeLib.fadeOut(bodyTag, true);
-    setTimeout(function() {
-      ipcRenderer.send('changePage', 'index.html');
-    }, fadeLib.fadeOutDelay);
+btnHome.addEventListener('click', function() {
+  fadeLib.fadeOut(bodyTag, true);
+  setTimeout(function() {
+    ipcRenderer.send('changePage', 'index.html');
+  }, fadeLib.fadeOutDelay);
 });
+
+btnNav.addEventListener('click', function() {
+  divNav.classList.toggle("show");
+});
+
+window.onclick = function(event) {
+    if (event.target == divNav) {
+        divNav.classList.remove("show");
+    }
+};
