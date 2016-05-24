e86f9c92c4cd8bf4e07bd45991b5c987fe46e471
ishmur <mateuszpab@gmail.com>
2016-05-24 23:02:13 +0200

    Print table in js

--- app/js/home.js
+++ app/js/home.js
@@ -24,7 +24,45 @@ btnHome.addEventListener('click', function() {
   }, funLib.fadeOutDelay);
 });
 
+function printTable(outputNode, stringArray, startingIndex, endingIndex, delimiterType){
+  var tableObject = document.createElement('table');
+  var tableHeadObject = document.createElement('thead');
+  var tableBodyObject = document.createElement('tbody');
+  var divObject = document.createElement('div');
 
+  tableObject.classList.add("table");
+  tableObject.classList.add("table-hover");
+
+  // Create table header
+  var outputDiv = outputNode.appendChild(divObject);
+  var tableHandle = outputDiv.appendChild(tableObject);
+  var tableHeadHandle = tableHandle.appendChild(tableHeadObject);
+
+  var tableRowArray = stringArray[startingIndex].trim();
+  tableRowArray = tableRowArray.split(delimiterType);
+  var tableRow = tableHeadHandle.insertRow(0);
+  for (var tableElementIterator in tableRowArray){
+    tableRowArray[tableElementIterator] = tableRowArray[tableElementIterator].trim();
+    var tableElement = tableRow.insertCell(tableElementIterator);
+    tableElement.innerHTML = tableRowArray[tableElementIterator];
+  }
+
+  // Fill table with data
+  var tableDataHandle = tableHandle.appendChild(tableBodyObject);
+  var rowNumber = 0;
+
+  for (var i=startingIndex+1; i < endingIndex; i++){
+    tableRowArray = stringArray[i].trim();
+    tableRowArray = tableRowArray.split(delimiterType);
+    tableRow = tableDataHandle.insertRow(rowNumber);
+    for (tableElementIterator in tableRowArray){
+      tableRowArray[tableElementIterator] = tableRowArray[tableElementIterator].trim();
+      tableElement = tableRow.insertCell(tableElementIterator);
+      tableElement.innerHTML = tableRowArray[tableElementIterator];
+    }
+    rowNumber++;
+  }
+}
 
 // Read file from input or drag
 
@@ -38,36 +76,21 @@ function handleFileSelect(evt) {
     var files = evt.target.files; // if input
   }
 
+  var outputDiv = document.getElementById("output");
+
+  while (outputDiv.firstChild) {
+    outputDiv.removeChild(outputDiv.firstChild);
+  }
+
   var reader = new FileReader();
 
   reader.onloadend = function(event) {
-    var tableObject = document.createElement('table');
-    var tableBodyObject = document.createElement('tbody');
-    tableObject.classList.add("table");
-    tableObject.classList.add("table-hover");
-    var divObject = document.createElement('div');
+
 
     var fileStringArray = this.result.split("\n");
 
-    var headerDiv = document.getElementById("output").appendChild(divObject);
-    headerDiv.innerHTML += "<h3>Naglowek pliku</h3>";
-    for (var i=0; i != 3; i++){
-      var headerText = fileStringArray[i] + "<br>";
-      headerDiv.innerHTML += headerText;
-    }
-
-    var tableHandle = headerDiv.appendChild(tableObject);
-    tableHandle = tableHandle.appendChild(tableBodyObject);
-
-    for (i=3; i != 5; i++){
-      var tableRowArray = fileStringArray[i].trim();
-      tableRowArray = tableRowArray.split("\t");
-      var tableRow = tableHandle.insertRow(i-3);
-      for (var tableElementIterator in tableRowArray){
-        var tableElement = tableRow.insertCell(tableElementIterator);
-        tableElement.innerHTML = tableRowArray[tableElementIterator];
-      }
-    }
+    printTable(outputDiv, fileStringArray, 0, 2, ",");
+    printTable(outputDiv, fileStringArray, 2, fileStringArray.length, "\t");
   };
 
   reader.readAsText(files[0]);
