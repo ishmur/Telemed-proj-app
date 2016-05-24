7c31a478215392faad196de1ef236e51e0ed0730
ishmur <mateuszpab@gmail.com>
2016-05-24 20:26:25 +0200

    Read File

--- app/js/home.js
+++ app/js/home.js
@@ -2,11 +2,14 @@
 
 const funLib = require('../app/js/library');
 const ipcRenderer = require('electron').ipcRenderer;
+const {dialog} = require('electron').remote;
 
 var btnHome = document.getElementById("btnHome");
 var bodyTag = document.getElementById("bodyTag");
-var divNav = document.getElementById("myNavbar");
-var btnNav = document.getElementsByClassName("navbar-toggle")[0];
+
+
+
+// Window fadeIn / fadeOut on load
 
 window.onload = function() {
   setTimeout(function() {
@@ -21,13 +24,61 @@ btnHome.addEventListener('click', function() {
   }, funLib.fadeOutDelay);
 });
 
-btnNav.addEventListener('click', function(event) {
-  divNav.classList.toggle("show");
-});
 
-window.onclick = function(event) {
-  // Function hiding the dropdown menu when clicked outside of its area
-  if (!funLib.climbUpDOM(event.target, "#myNavbar") && !funLib.climbUpDOM(event.target, ".navbar-toggle")) {
-    divNav.classList.remove("show");
+
+// Read file from input or drag
+
+function handleFileSelect(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  if(typeof evt.dataTransfer != 'undefined'){
+    var files = evt.dataTransfer.files; // if drag
+  } else{
+    var files = evt.target.files; // if input
   }
+
+  var reader = new FileReader();
+
+  reader.onloadend = function(event) {
+    var tableObject = document.createElement('table');
+    var tableBodyObject = document.createElement('tbody');
+    tableObject.classList.add("table");
+    tableObject.classList.add("table-hover");
+    var divObject = document.createElement('div');
+
+    var fileStringArray = this.result.split("\n");
+
+    var headerDiv = document.getElementById("output").appendChild(divObject);
+    headerDiv.innerHTML += "<h3>Naglowek pliku</h3>";
+    for (var i=0; i != 3; i++){
+      var headerText = fileStringArray[i] + "<br>";
+      headerDiv.innerHTML += headerText;
+    }
+
+    var tableHandle = headerDiv.appendChild(tableObject);
+    tableHandle = tableHandle.appendChild(tableBodyObject);
+
+    for (i=3; i != 5; i++){
+      var tableRowArray = fileStringArray[i].trim();
+      tableRowArray = tableRowArray.split("\t");
+      var tableRow = tableHandle.insertRow(i-3);
+      for (var tableElementIterator in tableRowArray){
+        var tableElement = tableRow.insertCell(tableElementIterator);
+        tableElement.innerHTML = tableRowArray[tableElementIterator];
+      }
+    }
+  };
+
+  reader.readAsText(files[0]);
 }
+
+function handleDragOver(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'send';
+}
+
+window.addEventListener('dragover', handleDragOver, false);
+window.addEventListener('drop', handleFileSelect, false);
+document.getElementById('file').addEventListener('change',handleFileSelect,false);
